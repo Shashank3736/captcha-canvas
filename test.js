af0e58cf627ad78a3482499fc0cebe2d9204df5d
Shashank <58896906+Shashank3736@users.noreply.github.com>
2020-09-14 08:01:49 +0530

    Update test.js

--- test.js
+++ test.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 
 const captcha = new CaptchaGenerator()
 new Promise(async (resolve, reject) => {
-    fs.writeFileSync(`./assets/captcha/default.png`, await captcha.create())
+    fs.writeFileSync(`./assets/captcha/default.png`, await captcha.generate())
 })
 //fs.writeFileSync('./assets/captcha/default.gif', captcha.createGif())
