e5aa4eb278bf31911f2960bba55c5cb2b69b906b
ShreyashKira <keleven64@gmail.com>
2020-09-17 19:50:21 +0530

    testing typescript support

---
+++ typings/index.d.ts
@@ -0,0 +1,37 @@
+declare module "captcha-canvas" {
+    export const version: string;
+    export class CaptchaGenerator {
+        constructor(options?: {height?: number, width?: number})
+        public height: number;
+        public width: number;
+        public captcha: SetCaptchaOptions;
+        public trace: SetTraceOptions;
+        public decoy: SetDecoyOptions;
+
+        public setBackgroud(image?: string | url);
+        public setDecoy(SetDecoyOptions);
+        public setTrace(SetTraceOptions);
+        public setCaptcha(SetCaptchaOptions);
+        public setDimension(height?: number, width?: number);
+        public generate(): Promise<Buffer>
+    }
+    interface SetCaptchaOptions {
+        characters?: number;
+        text?: string;
+        color?: string;
+        font?: string;
+        size?: number;
+        opacity?: number;
+    }
+    interface SetTraceOptions {
+        size?: number;
+        color?: string;
+        opacity?: number;
+    }
+    interface SetDecoyOptions {
+        color?: string;
+        opacity?: number;
+        font?: string;
+        size?: number;
+    }
+}
