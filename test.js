9e41e98b6c31cf4738f3089b0f96a88f12451eb0
Shashank <58896906+Shashank3736@users.noreply.github.com>
2020-10-11 12:38:46 +0530

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-const { CaptchaGenerator, resolveImage } = require("./");
-const fs = require('fs');
-(async() => {
-    const captcha = new CaptchaGenerator()
-    .generateSync()
-    
-    fs.writeFileSync("assets/captcha/default.png", captcha)
-})();
