279baba3d4dffdad7213a14b875b87a739f79a56
Ikramullah Latif <sky_m4n@outlook.com>
2020-10-16 15:58:42 +0800

    Fixing typings declaration and add example in typescript

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,7 +1,10 @@
-import { Image } from "canvas";
-
 declare module "captcha-canvas" {
+    import { Image } from "canvas";
     export const version: string;
+
+    /**
+     * Initatiates the creation of captcha image generation.
+     */
     export class CaptchaGenerator {
         constructor(options?: {height?: number, width?: number})
         public height: number;
@@ -10,12 +13,45 @@ declare module "captcha-canvas" {
         public trace: SetTraceOptions;
         public decoy: SetDecoyOptions;
 
-        public setBackgroud(image?: string);
-        public setDecoy(SetDecoyOptions);
-        public setTrace(SetTraceOptions);
-        public setCaptcha(SetCaptchaOptions);
-        public setDimension(height?: number, width?: number);
+        /**
+         * Get the text of captcha.
+         */
+        public text: string;
+
+        /**
+         * Set background for captcha image.
+         * @param image Buffer/url/path of image.
+         */
+        public setBackgroud(image: Buffer | string): CaptchaGenerator;
+        /**
+         * Change decoy options
+         * @param options Decoy characters customisation options
+         */
+        public setDecoy(options: SetDecoyOptions): CaptchaGenerator;
+        /**
+         * Change trace creation options.
+         * @param options Trace Line appearance options.
+         */
+        public setTrace(options: SetTraceOptions): CaptchaGenerator;
+        /**
+         * Change captcha text options
+         * @param options Captcha appearance options.
+         */
+        public setCaptcha(options: SetCaptchaOptions): CaptchaGenerator;
+        /**
+         * set dimension for your captcha image
+         * @param height Height of captcha image.
+         * @param width Width of captcha image.
+         */
+        public setDimension(height: number, width: number): CaptchaGenerator;
+        /**
+         * Method which returns image buffer
+         */
         public generate(): Promise<Buffer>;
+        /**
+         * Non asynchronous method to generate captcha image.
+         * @description It do not use setBackground method value for background image. If you want to set background and also use generateSync method then use background option in genrateSync method.
+         */
         public generateSync(options?: {background?: Image}): Buffer;
     }
     interface SetCaptchaOptions {
