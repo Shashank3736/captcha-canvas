b38381a6a5dd3ebe4fc76a763813e7f2d1de15f8
Shreyash Raj <shreyashr267@gmail.com>
2024-09-30 20:04:31 +0000

    removed createCaptchaSync

--- ts-script/extra.ts
+++ ts-script/extra.ts
@@ -1,26 +1,15 @@
 import { Captcha } from ".";
 import { CreateCaptchaOptions } from "./constants";
 
-interface captchaValueSync {
-    image: Buffer,
-    text: string
-}
-
-interface captchaValue {
-    image: Promise<Buffer>,
-    text: string
-}
-
 /**
  * Create custom captcha from scratch.
- * @async
- * @param {number} width Width of captcha image.
- * @param {number} height Height of captcha image.
  * @param {CreateCaptchaOptions} [option] Captcha text.
- * @returns 
+ * @returns { text: string }
  */
-export function createCaptcha(width: number, height: number, option: CreateCaptchaOptions = {}): captchaValue {
-    const captcha = new Captcha(width, height);
+export function createCaptcha(option: CreateCaptchaOptions) {
+    const captcha = new Captcha({ ctx: option.ctx });
+    const width = option.ctx.canvas.width;
+    const height = option.ctx.canvas.height;
     const decoyCount = Math.floor(width*height/2500);
 
     if(!option.decoy) option.decoy = {};
@@ -28,38 +17,11 @@ export function createCaptcha(width: number, height: number, option: CreateCaptc
 
     captcha.addDecoy(option.decoy);
 
-    
     captcha.drawCaptcha(option.captcha);
 
     captcha.drawTrace(option.trace);
 
     captcha.addDecoy({ opacity: 1 });
 
-    return { image: captcha.png, text: captcha.text };
-}
-/**
- * Create captcha in sync mode.
- * @param {number} width captcha image width.
- * @param {number} height captcha image height.
- * @param {CreateCaptchaOptions} [option] Captcha text.
- * @returns
- */
-export function createCaptchaSync(width: number, height: number, option: CreateCaptchaOptions = {}): captchaValueSync {
-    const captcha = new Captcha(width, height);
-    const decoyCount = Math.floor(width*height/2500);
-    captcha.async = false;
-
-    if(!option.decoy) option.decoy = {};
-    if(!option.decoy.total) option.decoy.total = decoyCount;
-
-    captcha.addDecoy(option.decoy);
-
-    
-    captcha.drawCaptcha(option.captcha);
-
-    captcha.drawTrace(option.trace);
-
-    captcha.addDecoy({ opacity: 1 });
-
-    return { image: captcha.png, text: captcha.text };
+    return { text: captcha.text };
 }
